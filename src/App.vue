a830fc14b62b7abc41c97d27d4288c9826aab0a2
pietroline <pietrobarberi@hotmail.it>
2022-02-21 16:35:37 +0100

    SingUpNow

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div>
 
     <header>
-      <LogoDC/>
-      <NavHeader/>
+      <div class="spazioCentro">
+        <LogoDC/>
+        <NavHeader/>
+      </div>
     </header>
 
     <main>
@@ -11,7 +13,15 @@
     </main>
 
     <footer>
-      <MyBlueBand/>
+      <div class="footerUp">
+        <div class="container"><MyBlueBand/></div>
+      </div>
+      <div class="footerDown">
+          <div class="container spazioCentro">
+            <SingUpNow/>
+            <SingUpNow/>
+          </div>
+      </div>
     </footer>
   
   </div>
@@ -22,6 +32,7 @@ import LogoDC from './components/LogoDC.vue';
 import NavHeader from './components/NavHeader.vue';
 import MyMain from './components/MyMain.vue';
 import MyBlueBand from './components/MyBlueBand.vue';
+import SingUpNow from './components/SingUpNow.vue';
 
 export default {
   name: 'App',
@@ -30,6 +41,7 @@ export default {
     NavHeader,
     MyMain,
     MyBlueBand,
+    SingUpNow,
   }
 }
 </script>
@@ -39,16 +51,38 @@ export default {
   @import "./assets/style/general.scss";
   @import "./assets/style/variables.scss";
 
+  .spazioCentro{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+  }
 
   header{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    @include centroPagina;
+       @include centroPagina;
   }
 
   main{
     background-color: $colorMain;
+  }
+
+  footer{
+
+    .footerUp{
+      background-color: $colorBlue;
+
+      .container{
+         @include centroPagina;
+      }
+    }
+
+    .footerDown{
+      background-color: $coloreNeroFooter;
+      padding: 2rem 0;
+
+      .container{
+         @include centroPagina;
+      }
+    }
+    
   }
 </style>
---
+++ src/components/SingUpNow.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="btn">SING-UP NOW!</div>
+</template>
+
+<script>
+    export default {
+        name: "SingUpNow",
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    @import "./../assets/style/variables.scss";
+
+    .btn{
+        border: 2px solid $colorBlue;
+
+        padding: 0.8rem;
+
+        color: #fff;
+
+        &:hover{
+            cursor: pointer;
+        }
+    }
+
+</style>
