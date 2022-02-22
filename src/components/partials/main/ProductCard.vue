dd3b159bc9c968cf17d4e3f8019ed2c1c448c186
pietroline <pietrobarberi@hotmail.it>
2022-02-22 16:37:53 +0100

    main parziale

---
+++ src/components/partials/main/ProductCard.vue
@@ -0,0 +1,40 @@
+<template>
+
+   <div  class="card">
+        <img :src="item.thumb" alt="">
+        <h5>{{item.series}}</h5>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "ProductCard",
+        props:["item"]
+    }
+</script>
+
+<style lang="scss" scope>
+
+    @import "./../../../assets/style/variables.scss";
+
+    .card{
+        width: calc(100% / 6);
+        padding: 1.5rem 0.8rem;
+
+        img{
+            width: 100%;
+            height: 10rem;
+            object-fit: cover;
+            object-position: top;
+        }
+
+        h5{
+            text-transform: uppercase;
+            color: $coloreTestoGrigio;
+
+            margin: 0.3rem 0;
+        }
+    }
+
+</style>
