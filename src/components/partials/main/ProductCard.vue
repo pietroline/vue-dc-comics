278a08687a09a799924c30595521a98198242dbd
pietroline <pietrobarberi@hotmail.it>
2022-02-22 17:19:03 +0100

    price on books

--- src/components/partials/main/ProductCard.vue
+++ src/components/partials/main/ProductCard.vue
@@ -1,8 +1,17 @@
 <template>
 
    <div  class="card">
-        <img :src="item.thumb" alt="">
+
+       <div class="contenitoreImg">
+
+            <a href="#">
+                <img :src="item.thumb" :alt="'img DC type ' + item.type">
+            </a>
+            <div class="price">{{item.price}}</div>
+
+       </div>
         <h5>{{item.series}}</h5>
+
     </div>
 
 </template>
@@ -21,6 +30,35 @@
     .card{
         width: calc(100% / 6);
         padding: 1.5rem 0.8rem;
+
+        &:hover h5{
+            color: #fff;
+        }
+
+        .contenitoreImg{
+            position: relative;
+        }
+
+        .price{
+            color: #fff;
+            font-weight: bold;
+            font-size: 2rem;
+
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+
+            display: none;
+        }
+        
+        &:hover .price{
+            display: block;
+        }
+
+        &:hover img{
+            opacity: 0.3;
+        }
 
         img{
             width: 100%;
